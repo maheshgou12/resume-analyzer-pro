ce20ed324a2ce5aff3021c788e5c93c8c3a0f5d8
Mahesh Burra <maheshburra2712@gmail.com>
2026-08-29 22:40:48 +0530

    fix: connect frontend to production backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://resume-analyzer-mahesh-api.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
